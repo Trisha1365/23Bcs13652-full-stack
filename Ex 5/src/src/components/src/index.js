98ad74e389cd329fdd2b0e395b35803429ec323c
Trisha1365 <trisha13652@gmail.com>
2025-09-09 13:37:18 +0530

    Delete Ex 5/src directory

--- Ex 5/src/src/components/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css"; // optional if you use Tailwind or your own CSS
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
